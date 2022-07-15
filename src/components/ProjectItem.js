b13c1a9b53ee0a61a7ca39b37655669340ec3873
Okubasufrank <franklineokubasu51@gmail.com>
2022-07-15 18:11:34 +0300

    Done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,11 +5,8 @@ function ProjectItem({ name, about, technologies }) {
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
     </div>
   );
 }
 
-export default ProjectItem;
+export default ProjectItem;;
